3601cdff45607a6a7969a6dbf9f97490a487f150
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-07-09 10:39:38 -0300

    Using lambda to calculate a multiplication table

---
+++ Lambda/src/main/java/br/com/leomanzini/multiplication/tables/MultiplicationTablesLambda.java
@@ -0,0 +1,23 @@
+package br.com.leomanzini.multiplication.tables;
+
+import java.util.Scanner;
+import java.util.function.UnaryOperator;
+
+public class MultiplicationTablesLambda {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter with a number to the multiplication tables: ");
+		int numberChoice = sc.nextInt();
+		
+		UnaryOperator<Integer> timeTables = value -> value * numberChoice;
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.printf("%d x %d = %d\n", numberChoice, i, timeTables.apply(i));
+		}
+		
+		sc.close();
+	}
+}
