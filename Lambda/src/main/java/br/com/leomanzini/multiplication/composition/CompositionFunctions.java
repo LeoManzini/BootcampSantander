17727b1a5b40e240a4279f792b5bdae78e4861e7
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-07-09 11:01:40 -0300

    Comparing two paradigms, functional and imperative

---
+++ Lambda/src/main/java/br/com/leomanzini/multiplication/composition/CompositionFunctions.java
@@ -0,0 +1,31 @@
+package br.com.leomanzini.multiplication.composition;
+
+import java.util.Arrays;
+
+public class CompositionFunctions {
+
+	public static void main(String[] args) {
+		
+		int[] values = {0, 1, 2, 3, 4};
+		
+		// Functional
+		Arrays.stream(values)
+			  .filter(number -> number % 2 == 0)
+			  .map(number -> number * 2)
+			  .filter(number -> number != 0)
+			  .forEach(number -> System.out.println(number));
+		
+		// Imperative
+		for(int i = 0; i < values.length; i++) {
+			int value = 0;
+			
+			if(values[i] % 2 == 0) {
+				value = values[i] * 2;
+				
+				if(value != 0) {
+					System.out.println(value);
+				}
+			}
+		}
+	}
+}
