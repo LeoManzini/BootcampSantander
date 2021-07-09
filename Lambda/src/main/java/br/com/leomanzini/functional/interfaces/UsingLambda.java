afaa5e835ce6cb81c73376cd914c6f51678e1ecc
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-07-09 12:28:03 -0300

    Adding a sample of more than just one line lambda

--- Lambda/src/main/java/br/com/leomanzini/functional/interfaces/UsingLambda.java
+++ Lambda/src/main/java/br/com/leomanzini/functional/interfaces/UsingLambda.java
@@ -4,8 +4,18 @@ public class UsingLambda {
 
 	public static void main(String[] args) {
 		
+		// One line logical lambda does not use {} and the returns clause is implicit
 		GenerateAbstractFunction insertSr = value -> "Sr. " + value;
 		
 		System.out.println(insertSr.generate("Leonardo"));
+		
+		// More than just one line, use {}; to delimit the scope, and have that specify the return clause
+		GenerateAbstractFunction insertSrAndDot = value -> {
+			String valueSr = "Sr. " + value;
+			String valueSrAndDot = valueSr + ".";
+			return valueSrAndDot;
+		};
+		
+		System.out.println(insertSrAndDot.generate("Leonardo"));
 	}
 }
