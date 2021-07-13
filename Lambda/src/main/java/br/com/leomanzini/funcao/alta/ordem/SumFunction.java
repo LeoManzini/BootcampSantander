a4d00956eb4771797fbcc9f17ce390632f084f02
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-07-12 22:40:26 -0300

    Adding more operations to the code

--- Lambda/src/main/java/br/com/leomanzini/funcao/alta/ordem/SumFunction.java
+++ Lambda/src/main/java/br/com/leomanzini/funcao/alta/ordem/SumFunction.java
@@ -11,8 +11,14 @@ public class SumFunction {
 		
 		// Definindo um comportamento
 		CalculateInterface sum = (x, y) -> x + y;
+		CalculateInterface subtraction = (x, y) -> x - y;
+		CalculateInterface multiplication = (x, y) -> x * y;
+		CalculateInterface division = (x, y) -> x / y;
 		
 		// Chamando a funcao passando a lambda de parametro com os valores (funcao)
 		System.out.println(executeOperation(sum, 5, 10));
+		System.out.println(executeOperation(subtraction, 15, 10));
+		System.out.println(executeOperation(multiplication, 5, 10));
+		System.out.println(executeOperation(division, 50, 10));
 	}
 }
