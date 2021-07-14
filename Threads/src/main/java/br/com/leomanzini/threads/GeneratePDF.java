6f0a44433cc8e9d952e4747155ff614ace131eb0
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-07-13 22:16:31 -0300

    Passing two classes to thread type

--- Threads/src/main/java/br/com/leomanzini/threads/GeneratePDF.java
+++ Threads/src/main/java/br/com/leomanzini/threads/GeneratePDF.java
@@ -1,9 +1,10 @@
 package br.com.leomanzini.threads;
 
-public class GeneratePDF implements Runnable {
+public class GeneratePDF extends Thread {
 
 	@Override
 	public void run() {
+		super.run();
 		System.out.println("Generating PDF file...");
 	}
 }
--- Threads/src/main/java/br/com/leomanzini/threads/LoadingBar.java
+++ Threads/src/main/java/br/com/leomanzini/threads/LoadingBar.java
@@ -1,9 +1,10 @@
 package br.com.leomanzini.threads;
 
-public class LoadingBar implements Runnable {
+public class LoadingBar extends Thread {
 
 	@Override
 	public void run() {
+		super.run();
 		System.out.println("Loading...");
 	}
 }
