38761936b17e71509073783ff566bdbc02376708
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-07-19 21:49:04 -0300

    Adding vector list implementation

--- CollectionsAndStreams/src/main/java/br/com/leomanzini/collections/list/ListsExample02.java
+++ CollectionsAndStreams/src/main/java/br/com/leomanzini/collections/list/ListsExample02.java
@@ -1,9 +1,35 @@
 package br.com.leomanzini.collections.list;
 
+import java.util.List;
+import java.util.Vector;
+
 public class ListsExample02 {
 
 	public static void main(String[] args) {
 		
+		List<String> sports = new Vector<>();
 		
+		// add four sports to the sports list
+		sports.add("Football");
+		sports.add("Basketball");
+		sports.add("Handball");
+		sports.add("Soccer");
+		
+		// modify the value at the vector second position
+		sports.set(0, "Ping-Pong");
+		
+		// remove the sport at the third position
+		sports.remove(2);
+		
+		// remove soccer from the array
+		sports.remove("Soccer");
+		
+		// return the first sports array value
+		System.out.println(sports.get(0));
+		
+		// navigate at sports
+		for(String sport : sports) {
+			System.out.println(sport);
+		}
 	}
 }
