533ed7747b20c34abbba0b62a1a62a030647be52
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-07-19 22:38:39 -0300

    Adding queue collection

---
+++ CollectionsAndStreams/src/main/java/br/com/leomanzini/collections/queue/QueueExample01.java
@@ -0,0 +1,40 @@
+package br.com.leomanzini.collections.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample01 {
+
+	public static void main(String[] args) {
+		
+		Queue<String> bankQueue = new LinkedList<>();
+		
+		bankQueue.add("Patrícia");
+		bankQueue.add("Roberto");
+		bankQueue.add("Flávio");
+		bankQueue.add("Pamela");
+		bankQueue.add("Anderson");
+		
+		System.out.println(bankQueue);
+		
+		String clientAtended = bankQueue.poll();
+		System.out.println("Next client to be atended: " + clientAtended);
+		System.out.println(bankQueue);
+		
+		String nextClient = bankQueue.peek();
+		System.out.println("Next client at the queue: " + nextClient);
+		System.out.println(bankQueue);
+		
+		// If we doesn't have elements at this queue, it throws an exception
+		// Works with loaders process and so on, where an empty queue is an error
+		
+		bankQueue.clear();
+		
+		nextClient = bankQueue.peek();
+		System.out.println(nextClient);
+		
+		String nextClientOrError = bankQueue.element();
+		System.out.println(nextClientOrError);
+		System.out.println(bankQueue);
+	}
+}
