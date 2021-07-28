9d90bfe93a816f2401d468786d3b5b9c95c7bbec
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-07-28 20:27:59 -0300

    Modifying some configurations

--- springbootconfig/src/main/java/br/com/leomanzini/springbootconfig/config/DbConfiguration.java
+++ springbootconfig/src/main/java/br/com/leomanzini/springbootconfig/config/DbConfiguration.java
@@ -1,12 +1,11 @@
 package br.com.leomanzini.springbootconfig.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import lombok.Getter;
-import lombok.Setter;
 
 @Configuration
 @ConfigurationProperties("spring.datasource")
@@ -15,25 +14,25 @@ import lombok.Setter;
 public class DbConfiguration {
 	
 	private String driverClassName;
-	private String url;
-	private String username;
-	private String password;
-	
-	@Profile("dev")
-	@Bean
-	public String testDatabaseConnection() {
-		System.out.println("DB Connection for DEV - H2");
-		System.out.println(driverClassName);
-		System.out.println(url);
-		return "DB Connection to H2_TEST - Test instance";
-	}
-	
-	@Profile("prod")
-	@Bean
-	public String productionDatabaseConnection() {
-		System.out.println("DB Connection for Production - MYSQL");
-		System.out.println(driverClassName);
-		System.out.println(url);
-		return "DB Connection to MYSQL_PROD - Production instance";
-	}
+    private String url;
+    private String username;
+    private String password;
+    
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection() {
+        System.out.println("DB connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MYSQL_TEST - Test instance";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String prodDatabaseConnection() {
+        System.out.println("DB connection for MYSQL Database: production");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MySql production - Test instance";
+    }
 }
