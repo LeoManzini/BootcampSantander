c095ece273f38f2990d74b3e0016f7edfae1ac1d
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-10 22:08:25 -0300

    Adding commentaries and a functional connection class

--- JDBC/src/main/java/br/com/leomanzini/jdbc/connections/PostgresConnector.java
+++ JDBC/src/main/java/br/com/leomanzini/jdbc/connections/PostgresConnector.java
@@ -1,5 +1,45 @@
 package br.com.leomanzini.jdbc.connections;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PostgresConnector {
 
+	public static void main(String[] args) {
+
+		// Step one: download connection jdbc driver for your connection
+
+		/* Step two: mount connection url jdbc:postgresql://127.0.0.1:5432/postgres
+		 * jdbc: Connection protocol
+		 * postgresql: Connection driver
+		 * 127.0.0.1: Host / Adress name
+		 * 5432: Port where my database is running
+		 * postgres: Database name (Where I want to connect)
+		 */
+		String urlConnection = "jdbc:postgresql://127.0.0.1:5432/postgres";
+		
+		// Step tree: Configure username and password to access the database
+		// PS: Not safety use the password hardcoded or via properties, use the same as a hash code
+		String user = "postgres";
+		String password = "281219";
+		Connection connection = null;
+
+		try {
+			
+			// Step four: DriveManager returns the connection to the database, using the url and the credentials
+			connection = DriverManager.getConnection(urlConnection, user, password);
+			System.out.println("Connected with success to PostgreSQL.");
+
+		} catch (Exception e) {
+			System.out.println("Could not connect to PostgreSQL.");
+		} 
+		finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
