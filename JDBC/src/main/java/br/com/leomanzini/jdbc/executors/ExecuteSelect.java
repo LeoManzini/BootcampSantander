da1f2578859d5add087fcdbd46a890e007052207
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-11 19:13:09 -0300

    Adding executor class

---
+++ JDBC/src/main/java/br/com/leomanzini/jdbc/executors/ExecuteSelect.java
@@ -0,0 +1,14 @@
+package br.com.leomanzini.jdbc.executors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ExecuteSelect implements Executors {
+
+	private static final Logger LOG = LogManager.getLogger(ExecuteSelect.class);
+	
+	@Override
+	public void execute(String propertiesPath) {
+		
+	}
+}
---
+++ JDBC/src/main/java/br/com/leomanzini/jdbc/executors/Executors.java
@@ -0,0 +1,6 @@
+package br.com.leomanzini.jdbc.executors;
+
+public interface Executors {
+	
+	public void execute(String propertiesPath);
+}
