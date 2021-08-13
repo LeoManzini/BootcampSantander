73f6897e5e8e9348880f74d66aaf8a3795a3a340
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-12 21:57:50 -0300

    Adding a modification at the main program

--- JPA/src/main/java/br/com/leomanzini/jpa/ApplicationHibernate.java
+++ JPA/src/main/java/br/com/leomanzini/jpa/ApplicationHibernate.java
@@ -7,12 +7,12 @@ import javax.persistence.Persistence;
 import br.com.leomanzini.jpa.dto.States;
 import br.com.leomanzini.jpa.dto.Students;
 
-public class Application {
+public class ApplicationHibernate {
 
 	public static void main(String[] args) {
 		
 		// Starts a entity with the persistence-unity configurations
-		EntityManagerFactory entityFactory = Persistence.createEntityManagerFactory("leomanzini-jpa-tests");
+		EntityManagerFactory entityFactory = Persistence.createEntityManagerFactory("leomanzini-hibernate");
 		EntityManager entityManager = entityFactory.createEntityManager();
 		
 		States state = new States("Rio Grande do Sul");
@@ -26,6 +26,24 @@ public class Application {
 		entityManager.persist(student);
 		
 		// Make a commit to the changes
+		entityManager.getTransaction().commit();
+		
+		// Looking for a specific class and ID, like a SELECT with where at the first id
+		States findState = entityManager.find(States.class, 1);
+		Students findStudent = entityManager.find(Students.class, 1);
+		
+		// Works because we already are inside of a transaction
+		System.out.println("State found: " + findState);
+		System.out.println("Student found: " + findStudent);
+		
+		// Each time that we alter something at the database, we have to start a new transaction
+		entityManager.getTransaction().begin();
+		
+		// Modify the objects
+		findStudent.setName("Luis Renato Manzini");
+		findStudent.setAge(45);
+		
+		// Commit the modifications at database
 		entityManager.getTransaction().commit();
 		
 		// Close the open entities
