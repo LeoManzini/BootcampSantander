f8826b117a3b4a6148787716eb11750c7bc0765e
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-16 15:37:44 -0300

    Adding a consult with native sql

---
+++ JPA/src/main/java/br/com/leomanzini/jpa/jpql/ApplicationJpql.java
@@ -0,0 +1,41 @@
+package br.com.leomanzini.jpa.jpql;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.leomanzini.jpa.dto.Students;
+
+public class ApplicationJpql {
+
+	public static void main(String[] args) {
+
+		// Starts a entity with the persistence-unity configurations
+		EntityManagerFactory entityFactory = Persistence.createEntityManagerFactory("leomanzini-hibernate");
+		EntityManager entityManager = entityFactory.createEntityManager();
+
+		// Name used at the next consults
+		String name = "Giovanna";
+		
+		// Open a database connection
+		entityManager.getTransaction().begin();
+		
+		// Find a specific student at database, but if we want to find more than one, it's impossible just with jpa and hibernate
+		Students findStudent = entityManager.find(Students.class, 1);
+		
+		System.out.println(findStudent);
+		
+		// Creating a SQL query native, returns just one line too
+		String sql = "SELECT * FROM Students WHERE name = :name ";
+		Students sqlStudent = (Students) entityManager.createNativeQuery(sql, Students.class).setParameter("name", name).getSingleResult();
+		
+		// Returns a list as query result
+		String sqlList = "SELECT * FROM Students";
+		List<Students> studentsSqlList = entityManager.createNativeQuery(sqlList, Students.class).getResultList();
+		
+		System.out.println("One line query return: " + sqlStudent);
+		studentsSqlList.stream().forEach(Student -> System.out.println("Sql list consult: " + Student));
+	}
+}
