4c55adb7f4e21edde0026f9605364e1f2e692473
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-16 17:12:44 -0300

    Adding a consult with criteria API

--- JPA/src/main/java/br/com/leomanzini/jpa/jpql/ApplicationJpql.java
+++ JPA/src/main/java/br/com/leomanzini/jpa/jpql/ApplicationJpql.java
@@ -5,7 +5,11 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
+import br.com.leomanzini.jpa.dto.States;
 import br.com.leomanzini.jpa.dto.Students;
 
 public class ApplicationJpql {
@@ -48,6 +52,36 @@ public class ApplicationJpql {
 		List<Students> studentsJpqlList = entityManager.createQuery(jpqlList, Students.class).getResultList();
 		
 		System.out.println("One line query return: " + jpqlStudent);
-		studentsJpqlList.stream().forEach(Student -> System.out.println("Sql list consult: " + Student));
+		studentsJpqlList.stream().forEach(Student -> System.out.println("Jpql list consult: " + Student));
+		
+		// One another advantage, if we want to do the example bellow with just SQL we need to make joins and a lot of stuf
+		// but with JPQL we just need to use the classes and objects, no matter how de database are, just the objects structures and mapping
+		jpqlList = "SELECT a FROM Atudents WHERE a.state.name = :stateName";
+		List<Students> jpqlConditionList = entityManager.createQuery(jpqlList, Students.class).setParameter("stateName", "Sao Paulo").getResultList();	
+		jpqlConditionList.stream().forEach(Student -> System.out.println("Conditional JPQL list consult: " + Student));
+		
+		jpqlConditionList.clear();
+		
+		// You can use an object to looking for too, like this:
+		jpqlList = "SELECT a FROM Atudents WHERE a.state = :state";
+		// EntityManager will find with a SELECT the state with primary key being 1 and use it at the outside query
+		jpqlConditionList = entityManager.createQuery(jpqlList, Students.class).setParameter("state", entityManager.find(States.class, 1)).getResultList();
+		
+		// JPA Criteria API
+		// Returning just one result
+		// CriteriaQuery<Students> criteria = entityManager.getCriteriaBuilder().createQuery(Students.class);
+		// Root<Students> rootStudent = criteria.from(Students.class);
+		// CriteriaBuilder.In<String> inClause = entityManager.getCriteriaBuilder().in(rootStudent.get(Students_.name)); // Students_ is the JPA metamodel
+		// inClause.value(name);
+		// criteria.select(rootStudent).where(inClause);
+		// Students criteriaApiStudent = entityManager.createQuery(criteria).getSingleResult();
+		// System.out.println("Student using criteria: " + criteriaApiStudent);
+		
+		// Returning a Criteria API list
+		CriteriaQuery<Students> criteriaList = entityManager.getCriteriaBuilder().createQuery(Students.class);
+		Root<Students> rootListStudents = criteriaList.from(Students.class);
+		List<Students> studentsCriteriaList = entityManager.createQuery(criteriaList).getResultList();
+		
+		studentsCriteriaList.stream().forEach(Student -> System.out.println("JPA Criteria API list consult: " + Student));
 	}
 }
