a1180e334fdd8f4b678b4dc83864e5842c3994ec
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-26 21:59:43 -0300

    Adding the table of courses to the html

--- Angular/course-manager/src/app/courses/course-list.component.ts
+++ Angular/course-manager/src/app/courses/course-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
 
 @Component({
@@ -6,7 +6,32 @@ import { Course } from "./course";
     templateUrl: './course-list.components.html'
 })
 
-export class CourseListComponents {
+export class CourseListComponents implements OnInit {
 
     courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 4.8,
+                releaseDate: 'September, 26, 2020'
+            }, 
+            {
+                id: 1,
+                name: 'Java 11',
+                imageUrl: '',
+                price: 199.99,
+                code: 'XPS-8797',
+                duration: 200,
+                rating: 5.0,
+                releaseDate: 'December, 21, 2021'
+            }
+        ]
+    }
 }
