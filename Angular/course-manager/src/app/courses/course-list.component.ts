643c1930208f6b31dffe567c54a5b9b1991c4143
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-27 17:08:42 -0300

    Adding an injectable class

--- Angular/course-manager/src/app/courses/course-list.component.ts
+++ Angular/course-manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -10,28 +11,9 @@ export class CourseListComponents implements OnInit {
 
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService) {}
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'September, 26, 2020'
-            }, 
-            {
-                id: 1,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 199.99,
-                code: 'XPS-8797',
-                duration: 200,
-                rating: 4.0,
-                releaseDate: 'December, 21, 2021'
-            }
-        ]
+        this.courses = this.courseService.retrieveAll();
     }
 }
